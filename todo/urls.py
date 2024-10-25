c035d14d7847f72092663b126c6c8cad2d83fb6c
AbdullahAlNaim <aanaim96@gmail.com>
2024-10-25 19:13:41 -0400

    create task attempts

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index.as_view()),
+    path('', views.IndexView.as_view()),
+    path('created', views.CreatedView.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,28 +2,15 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import Task
 from .forms import TaskForm
+from django.views.generic.edit import CreateView
+
 # Create your views here.
 
-class index(TemplateView):
+class IndexView(CreateView):
+    model = Task
+    form_class = TaskForm
     template_name = 'todo/index.html'
-    task_form = TaskForm
+    success_url = '/created'
 
-    def get(self, request):
-        tasks = Task.objects.all()
-        return render(request, self.template_name, {
-            'tasks': tasks
-        })
-
-    def post(self, request):
-        form = self.task_form(request.POST)
-        tasks = Task.objects.all()
-        if form.is_valid():
-            form.save()
-            tasks = Task.objects.all()
-            return render(request, self.template_name, {
-                'tasks': tasks
-            })
-
-        return render(request, self.template_name, {
-            'tasks': tasks
-        })
+class CreatedView(TemplateView):
+    template_name = 'todo/task_created.html'
