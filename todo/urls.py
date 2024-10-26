d51e0dd02b3ea78746f0d51e97d46bf2bf21ff1d
AbdullahAlNaim <aanaim96@gmail.com>
2024-10-26 11:14:53 -0400

    able to add tasks to todo app

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('created', views.CreatedView.as_view()),
+    path('', views.IndexView.as_view(), name='index'),
+    path('created', views.CreatedView.as_view(), name='created'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,15 +2,37 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import Task
 from .forms import TaskForm
+from django.views.generic.edit import FormView
 from django.views.generic.edit import CreateView
+from django.views import View
 
 # Create your views here.
 
-class IndexView(CreateView):
-    model = Task
-    form_class = TaskForm
+class IndexView(View):
     template_name = 'todo/index.html'
-    success_url = '/created'
+    def get(self, request):
+        tasks = Task.objects.all()
+        form = TaskForm()
+        return render(request, self.template_name, {
+            'tasks': tasks,
+            'form': form
+        })
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+        tasks = Task.objects.all()
+        return render(request, self.template_name, {
+            'form': form,
+            'tasks': tasks
+        })
+    # model = Task
+    # form_class = TaskForm
+    # template_name = 'todo/index.html'
+    # success_url = '/created'
 
 class CreatedView(TemplateView):
     template_name = 'todo/task_created.html'
